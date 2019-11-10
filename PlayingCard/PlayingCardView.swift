7045f0b4aeda0d6c6ffab7511afeae9ac355f274
YesVladess <vlad.kononenko95@gmail.com>
2019-11-11 02:07:21 +0300

    App prepared for starting animating playingCards

--- PlayingCard/PlayingCardView.swift
+++ PlayingCard/PlayingCardView.swift
@@ -8,140 +8,68 @@
 
 import UIKit
 
+/// Playing card view that contains a suit and a rank. It can be facing
+/// up or down.
 @IBDesignable
 class PlayingCardView: UIView {
-
-    @IBInspectable
-    var rank : Int = 7 { didSet { setNeedsLayout(); setNeedsDisplay() }}
-    @IBInspectable
-    var suit : String = "♠️" { didSet { setNeedsLayout(); setNeedsDisplay() }}
-    @IBInspectable
-    var isFaceUp : Bool = true { didSet { setNeedsLayout(); setNeedsDisplay() }}
     
-    var faceCardScale : CGFloat = SizeRatio.faceCardImageSizeToBoundsSize { didSet { setNeedsDisplay() }}
+    /// The cards rank
+    @IBInspectable
+    var rank: Int = 9 { didSet{ updateView() } }
     
-    @objc func adjustFaceCardScale(byHandlingGestureRecognizerBy recognizeer : UIPinchGestureRecognizer) {
-        switch recognizeer.state {
+    /// The cards suit
+    @IBInspectable
+    var suit: String = "♥️" { didSet{ updateView() } }
+    
+    /// Whether or not the card is facing up
+    @IBInspectable
+    var isFaceUp: Bool = false { didSet{ updateView() } }
+    
+    /// The scale of the face card
+    var faceCardScale: CGFloat = SizeRatio.faceCardImageSizeToBoundsSize { didSet { updateView() } }
+    
+    /// Handle pinch to zoom
+    @objc func adjustFaceCardScale(gestureRecognizer: UIPinchGestureRecognizer) {
+        switch gestureRecognizer.state {
         case .changed, .ended:
-            faceCardScale *= recognizeer.scale
-            recognizeer.scale = 1.0
+            faceCardScale *= gestureRecognizer.scale
+            gestureRecognizer.scale = 1.0 // reset it to get incremental changes only
         default:
             break
         }
     }
-    
-    private func centeredAttributedString( _ string: String, fontSize: CGFloat) -> NSAttributedString {
-        
-        var font = UIFont.preferredFont(forTextStyle: .body).withSize(fontSize)
-        font = UIFontMetrics(forTextStyle: .body).scaledFont(for: font)
-        let paragraphStyle = NSMutableParagraphStyle()
-        paragraphStyle.alignment = .center
-        return NSAttributedString(string: string, attributes: [.paragraphStyle: paragraphStyle, .font: font])
-    }
-    
-    private var cornerString : NSAttributedString {
-        return centeredAttributedString(rankString+"\n"+suit, fontSize: cornerFontSize)
-    }
-    
-    /// The label for the upper-left corner of the card
-    private lazy var upperLeftCornerLabel = createCornerLabel()
-    
-    /// The label for the bottom-right corner of the card
-    private lazy var lowerRightCornerLabel = createCornerLabel()
-    
-    /// The origin point for the upper-left corner label
-    private var upperLeftCornerLabelOrigin: CGPoint {
-        return bounds.origin.offsetBy(dx: cornerOffset, dy: cornerOffset)
-    }
-    
-    /// The origin point for the lower-right corner label
-    private var lowerRightCornerLabelOrigin: CGPoint {
-        return CGPoint(x: bounds.maxX, y: bounds.maxY)
-            .offsetBy(dx: -cornerOffset, dy: -cornerOffset)
-            .offsetBy(dx: -lowerRightCornerLabel.frame.width, dy: -lowerRightCornerLabel.frame.height)
-    }
-    
-    /// When bounds change, we want to keep everything positioned correctly
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        
-        // Configure corner labels
-        configureCornerLabel(upperLeftCornerLabel)
-        configureCornerLabel(lowerRightCornerLabel)
-        
-        // Position corner labels accordingly (using computed vars instead of calculating it here)
-        upperLeftCornerLabel.frame.origin = upperLeftCornerLabelOrigin
-        lowerRightCornerLabel.frame.origin = lowerRightCornerLabelOrigin
-        
-        // NOTE: On the lecture, the professor does the transformation first and then sets the label's origin.
-        // Because of this, the transformation needs both "translation" and "rotation". We can also set the origin
-        // first and then applying a "rotation" only, which I'm doing here.
-        
-        // We need to rotate the lower-right label
-        lowerRightCornerLabel.transform = CGAffineTransform.identity.rotated(by: CGFloat.pi)
-        
-    }
-    
-    /// Creates the corner label
-    private func createCornerLabel() -> UILabel {
-        
-        let label = UILabel()
-        // Zero means "no-limit"
-        label.numberOfLines = 0
-        addSubview(label)
-        return label
-    }
-    
-    /// Configure the corner labels text, size, etc.
-    private func configureCornerLabel(_ label: UILabel) {
-        // Set the attributed text
-        label.attributedText = cornerString
-        
-        // Set the label's size to fit the content
-        label.frame.size = CGSize.zero // reset it's size first
-        label.sizeToFit()
-        
-        // Only show the label when the card is facing-up
-        label.isHidden = !isFaceUp
-    }
-    
+
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
     override func draw(_ rect: CGRect) {
-// Drawing with Core Graphics
-//        if let context = UIGraphicsGetCurrentContext() {
-//            context.addArc(center: CGPoint(x: bounds.midX, y: bounds.midY),
-//            radius: 100.0, startAngle: 0, endAngle: 2*CGFloat.pi, clockwise: false)
-//            context.setLineWidth(5.0)
-//            UIColor.green.setStroke()
-//            UIColor.blue.setFill()
-//            context.strokePath()
-//            // it's not filling
-//            context.fillPath()
-//        }
-// Drawing with UIBezierPath
-//        let path = UIBezierPath()
-//        path.addArc(withCenter: CGPoint(x: bounds.midX, y: bounds.midY),
-//                    radius: 100.0, startAngle: 0, endAngle: 2*CGFloat.pi, clockwise: false)
-//        path.lineWidth = 5.0
-//        UIColor.green.setStroke()
-//        UIColor.blue.setFill()
-//        path.stroke()
-//        path.fill()
-        
         let roundedRect = UIBezierPath(roundedRect: bounds, cornerRadius: cornerRadius)
         roundedRect.addClip()
         UIColor.white.setFill()
+        UIColor.lightGray.setStroke()
         roundedRect.fill()
-        if isFaceUp{
-        if let faceCardImage = UIImage(named: rankString + suit, in: Bundle(for: self.classForCoder), compatibleWith: traitCollection) {
-            faceCardImage.draw(in: bounds.zoom(by: faceCardScale))
-        } else {
-            drawPips()
+        roundedRect.stroke()
+        
+        if isFaceUp {
+            // If card has an image, show it (i.e. for Jack/Queen/King)
+            if let faceCardImage = namedImage("\(rankString)\(suit)") {
+                faceCardImage.draw(in: bounds.zoom(by: faceCardScale))
+            }
+                // There is no image, draw the pips
+            else {
+                drawPips()
+            }
         }
-        } else {
-            if let cardBackImage = UIImage(named: "cardback", in: Bundle(for: self.classForCoder), compatibleWith: traitCollection) {
+        else {
+            if let cardBackImage = namedImage("cardback") {
                 cardBackImage.draw(in: bounds)
             }
         }
+    }
+
+    /// Returns UIImage from the given name (similar to UIImage(named:), but also shows
+    /// on Interface Builder)
+    private func namedImage(_ name: String) -> UIImage? {
+        return UIImage(named: name, in: Bundle(for: self.classForCoder), compatibleWith: traitCollection)
     }
     
     /// Draw pips based on rank and suit
@@ -185,16 +113,114 @@ class PlayingCardView: UIView {
             }
         }
     }
+
+    /// Get an NSAttributedString with the given string and size
+    private func centeredAttributedString(_ string: String, fontSize: CGFloat) -> NSAttributedString {
+        // Create font with given size
+        var font = UIFont.preferredFont(forTextStyle: .body).withSize(fontSize)
+        
+        // Scale the font to what the user's device has (i.e. Settings>Accessibility>FontSize might be set
+        // to prefer bigger/smaller font sizes)
+        font = UIFontMetrics(forTextStyle: .body).scaledFont(for: font)
+        
+        // Center the font
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.alignment = .center
+        
+        // The attributes to build our string
+        let attributes: [NSAttributedString.Key:Any] = [
+            .paragraphStyle : paragraphStyle,
+            .font : font
+        ]
+        
+        // Create it and return it
+        return NSAttributedString(string: string, attributes: attributes)
+    }
     
-    /// Called when the iOS interface environment changes.
-    ///
-    /// For example, when user changes the system-wide accessibility font size
+    /// Returns the string that goes on the card's corner
+    private var cornerString: NSAttributedString {
+        return centeredAttributedString("\(rankString)\n\(suit)", fontSize: cornerFontSize)
+    }
+    
+    /// Updates the view
+    private func updateView() {
+        setNeedsDisplay()
+        setNeedsLayout()
+    }
+    
+    /// The label for the upper-left corner of the card
+    private lazy var upperLeftCornerLabel = createCornerLabel()
+    
+    /// The label for the bottom-right corner of the card
+    private lazy var lowerRightCornerLabel = createCornerLabel()
+    
+    /// Creates the corner label
+    private func createCornerLabel() -> UILabel {
+        let label = UILabel()
+        
+        // Zero means "no-limit"
+        label.numberOfLines = 0
+        addSubview(label)
+        
+        return label
+    }
+    
+    /// When bounds change, we want to keep everything positioned correctly
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        // Configure corner labels
+        configureCornerLabel(upperLeftCornerLabel)
+        configureCornerLabel(lowerRightCornerLabel)
+
+        // Position corner labels accordingly (using computed vars instead of calculating it here)
+        upperLeftCornerLabel.frame.origin = upperLeftCornerLabelOrigin
+        lowerRightCornerLabel.frame.origin = lowerRightCornerLabelOrigin
+        
+        // NOTE: On the lecture, the professor does the transformation first and then sets the label's origin.
+        // Because of this, the transformation needs both "translation" and "rotation". We can also set the origin
+        // first and then applying a "rotation" only, which I'm doing here.
+        
+        // We need to rotate the lower-right label
+        lowerRightCornerLabel.transform = CGAffineTransform.identity.rotated(by: CGFloat.pi)
+
+    }
+    
+    /// The origin point for the upper-left corner label
+    private var upperLeftCornerLabelOrigin: CGPoint {
+        return bounds.origin.offsetBy(dx: cornerOffset, dy: cornerOffset)
+    }
+    
+    /// The origin point for the lower-right corner label
+    private var lowerRightCornerLabelOrigin: CGPoint {
+        return CGPoint(x: bounds.maxX, y: bounds.maxY)
+            .offsetBy(dx: -cornerOffset, dy: -cornerOffset)
+            .offsetBy(dx: -lowerRightCornerLabel.frame.width, dy: -lowerRightCornerLabel.frame.height)
+    }
+    
+    /// Configure the corner labels text, size, etc.
+    private func configureCornerLabel(_ label: UILabel) {
+        // Set the attributed text
+        label.attributedText = cornerString
+        
+        // Set the label's size to fit the content
+        label.frame.size = CGSize.zero // reset it's size first
+        label.sizeToFit()
+        
+        // Only show the label when the card is facing-up
+        label.isHidden = !isFaceUp
+    }
+    
+    // Called when the iOS interface environment changes.
+    //
+    // For example, when user changes the system-wide accessibility font size
     override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
         super.traitCollectionDidChange(previousTraitCollection) // NOTE: professor forgot to call super's method
-        //updateView()
+        updateView()
     }
     
 }
+
 
 // Extension with simple but useful utilities
 extension PlayingCardView {
--- PlayingCard/ViewController.swift
+++ PlayingCard/ViewController.swift
@@ -8,48 +8,40 @@
 
 import UIKit
 
+/// Main view controller
 class ViewController: UIViewController {
     
-    var deck = PlayingCardDeck()
-
-    @IBOutlet weak var playingCardView: PlayingCardView! {
-        didSet {
-            let swipe = UISwipeGestureRecognizer(target: self, action: #selector(nextCard))
-            swipe.direction = [.left, .right]
-            playingCardView.addGestureRecognizer(swipe)
-            let pinch = UIPinchGestureRecognizer(target: playingCardView, action: #selector(PlayingCardView.adjustFaceCardScale(byHandlingGestureRecognizerBy:)))
-            playingCardView.addGestureRecognizer(pinch)
-        }
-    }
+    /// The deck of cards (model)
+    private var deck = PlayingCardDeck()
     
-    @IBAction func flipCard(_ sender: UITapGestureRecognizer) {
-        switch sender.state {
-        case .ended:
-            playingCardView.isFaceUp = !playingCardView.isFaceUp
-        default:
-            break
-        }
-        
-    }
-    
-    @objc func nextCard() {
-        if let card = deck.draw() {
-            playingCardView.rank = card.rank.order
-            playingCardView.suit = card.suit.rawValue
-        }
-    }
+    /// The playing cards views (view)
+    @IBOutlet private var cardViews: [PlayingCardView]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//      For debug
-//        for _ in 1...10 {
-//            if let card = deck.draw() {
-//                print("\(card)")
-//            }
-//        }
-//      Debug end
+        var cards = [PlayingCard]()
+        for _ in 1...((cardViews.count+1)/2) {
+            let card = deck.draw()!
+            cards += [card, card]
+        }
+        for cardView in cardViews {
+            cardView.isFaceUp = false
+            let card = cards.remove(at: cards.count.randomNumber)
+            cardView.rank = card.rank.order
+            cardView.suit = card.suit.rawValue
+            cardView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(flipCard(_:))))
+        }
     }
-
-
+    
+    @objc private func flipCard(_ recognizer: UITapGestureRecognizer) {
+        switch recognizer.state {
+        case .ended:
+            if let chosenCardView = recognizer.view as? PlayingCardView {
+                chosenCardView.isFaceUp = !chosenCardView.isFaceUp
+            }
+        default:
+            break
+        }
+    }
 }
 
